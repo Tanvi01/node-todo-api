adc3d8169900e0bc9451c552827ccf4df0b122c7
Tanvi01 <gtanvi24@gmail.com>
2018-07-21 01:51:28 +0530

    'Initial'

---
+++ playground/mongodb-find.js
@@ -0,0 +1,14 @@
+//const MongoClient=require('mongodb').MongoClient;
+const {MongoClient,ObjectID}=require('mongodb');
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+	if (err)
+		return console.log('unable to connect to mongodb server');
+	console.log('Connected to mongodb server');
+db.collection('Todos').find().toArray().then((docs)=>{
+	console.log('Todos');
+	console.log(JSON.stringify(docs,undefined,2));
+},(err)=>{
+	console.log('Unable to fetch the todos',err);
+});
+	//db.close();
+});
